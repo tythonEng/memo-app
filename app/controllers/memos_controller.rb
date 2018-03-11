6931b59ef060e5383d82ffe615d0d07832074b41
tadayuki <t.yokota.eng@gmail.com>
2018-03-11 18:12:42 +0900

    Add posting memo function (no Ajax)

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,2 +1,17 @@
 class MemosController < ApplicationController
+  def index
+    @memos = Memo.all
+    @memo = Memo.new
+  end
+
+  def create
+    Memo.create(memo_params)
+    redirect_to :root
+  end
+
+  private
+  def memo_params
+    params.require(:memo).permit(:text)
+  end
+
 end
